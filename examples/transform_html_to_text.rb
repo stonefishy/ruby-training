c872feec58ec0835901718b16f352e1e15aabd4a
stonefishy <andrewsy@163.com>
2015-11-18 15:58:37 +0800

    only get the body content

--- examples/transform_html_to_text.rb
+++ examples/transform_html_to_text.rb
@@ -7,3 +7,21 @@ File.open(file_name, 'w') do |file|
 	text = open(url).read
 	file.write text
 end
+
+text_name = "ruby-doc.text"
+html = File.read(file_name)
+
+File.open(text_name, 'w') do |file|
+	in_header = true
+	html.each_line do |line|
+		if in_header && /<body/ !~ line
+			next
+		else
+			in_header = false
+		end
+
+		file.write line
+		
+		break if /<\/body/ =~ line
+	end
+end
